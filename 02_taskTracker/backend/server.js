d22bcb9e2ee2be21ff3e49f42c8452d8c69fc4d4
Ayush <ayushkumar85385@gmail.com>
2025-09-05 10:59:10 +0530

    added cors

--- 02_taskTracker/backend/server.js
+++ 02_taskTracker/backend/server.js
@@ -2,12 +2,14 @@ import router from './routes/routes.js';
 import express from 'express';
 import dotenv from 'dotenv';
 import {connectDB} from './db/connectDB.js';
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', router);
 
